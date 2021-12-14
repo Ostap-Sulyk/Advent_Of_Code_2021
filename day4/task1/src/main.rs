1da1b34a68d8afe0f58e211e436b2205f90b3401
Ostap-Sulyk <ostapsulyk@gmail.com>
2021-12-13 23:16:10 -0500

    added ability to find last index needed per row

--- day4/task1/src/main.rs
+++ day4/task1/src/main.rs
@@ -1,6 +1,4 @@
-use std::cell;
-
-#[allow(dead_code)]
+#[allow(dead_code, unused_mut)]
 fn main() {
     let raw_data: Vec<&str> = include_str!("../test_input").split("\n").collect();
     // println!("{:}", raw_data);
@@ -31,16 +29,73 @@ fn main() {
 
     let mut cell_index = 0;
 
-    for i in 0..cells.len() / 5 {
+    for _i in 0..cells.len() / 5 {
         let mut board = Vec::new();
         {
-            let mut line = [0; 5];
+            let mut line = [(0, false); 5];
             for i in 0..5 {
-                line[i] = cells[cell_index];
+                line[i] = (cells[cell_index], false);
                 cell_index += 1;
             }
             board.push(line);
         }
         boards.push(board);
+    }
+
+    // iterate over
+    // iterating over each boards row and column
+    // and finding index from random numbers at witch all rows or columns are filled
+
+    let mut when_bingo_happened: usize; // will hold the biggest index from random_numbers
+    let mut current_last_index_per_board: usize;
+    let mut current_last_index_per_row = -1;
+    let mut current_last_index_per_column = -1;
+    let mut current_last_index_per_cell = -1;
+    let mut index_of_winning_board: usize;
+
+    let mut couner = 0;
+    for board in boards.iter() {
+        for row in board {
+            for cell in row {
+                for (index, number) in _random_numbers.iter().enumerate() {
+                    if cell.0 == *number {
+                        if current_last_index_per_cell < index as i32 {
+                            current_last_index_per_cell = index as i32;
+                        }
+                    }
+                }
+            }
+        }
+        if current_last_index_per_row < current_last_index_per_cell {
+            current_last_index_per_row = current_last_index_per_cell
+        }
+        if couner == 4 {
+            break;
+        } else {
+            couner += 1;
+        }
+    }
+
+    println!(
+        "Current last index per first row in first table is {}",
+        current_last_index_per_row
+    );
+
+    let mut couner = 0;
+    'outer: for board in boards.iter() {
+        for row in board {
+            for (index, boolean) in row {
+                println!(
+                    "{}",
+                    _random_numbers.iter().position(|&x| x == *index).unwrap()
+                )
+            }
+        }
+        println!();
+        if couner == 4 {
+            break;
+        } else {
+            couner += 1;
+        }
     }
 }
